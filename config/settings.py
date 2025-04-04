a836affb194289e9a80d297890466e0cec8b0b17
Samuel Dominguez <onlinebusinessfy@gmail.com>
2025-04-03 17:51:26 -0700

    Heroku deployment

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-5k=3n7fo_=_%=!fpq9$sv(rs+)%*zkzp(xol$e44knpkxmsf0u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".herokuapp.com"
+]
 
 
 # Application definition
